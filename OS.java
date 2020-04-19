8ed837cac648aeb13dc15e28cc4950a433a3cdd1
AbdulazizQ3382 <50684369+AbdulazizQ3382@users.noreply.github.com>
2020-04-19 23:04:25 +0300

    Add files via upload

--- OS.java
+++ OS.java
@@ -1,7 +1,7 @@
 package csc227Project;
 
-public class OS {
-	public Hardware h;
+public  class OS {
+	public static Hardware h;
 
 	public OS(Hardware h) {
 		this.h = h;
@@ -18,6 +18,44 @@ public class OS {
 		}
 	return maxp.processId;
 }
+	public process[] sjf() {
+		process sjf[]=new process[h.numProcess];
+		for(int i=0;i<h.numProcess;i++) {
+			sjf[i]=h.processes.get(i);
+			timeLoadedInReadyQueue(sjf[i]);
+		}
+	   return sjf;
+	}
+	
+	public int timeTerminatedOrKilled(process p) {
+		if(p.state.terminate==true || p.state.kill==true)
+			return p.clock;
+		else
+			return 0 ;
+	}
+	public boolean isTerminated(process p) {
+		return p.state.terminate==true;
+	}
+	public boolean isKilld(process p) {
+		return p.state.kill==true;
+	}
+		
+    public int timeLoadedInReadyQueue(process p) {
+    	return p.clock;
+    	
+    }
+    // true if p1 is smaller than p2
+    public boolean checkBurst(process p1 , process p2) {
+    	if(p1.cpuBurst<p2.cpuBurst)
+    		return true;
+    	else
+    		return false;
+    }
+		
+	
+
+//	
+	
 }
 
 
