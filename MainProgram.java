17590cf106e4d651583436fb86d932150d5a5837
AbdulazizQ3382 <50684369+AbdulazizQ3382@users.noreply.github.com>
2020-04-19 23:51:25 +0300

    Add files via upload

---
+++ MainProgram.java
@@ -0,0 +1,67 @@
+package csc227Project;
+import java.io.*;
+import java.util.*;
+
+public class MainProgram {
+
+	public static void main(String args[]) throws FileNotFoundException {
+		int timeTerminated=0;
+		LinkedList<process> readyQueue2=new LinkedList<process>();
+	    process pro[]=new process[5];
+		for(int i=0; i<5;i++)
+	    	pro[i]=new process(i);
+		Hardware h = new Hardware();
+		for(int i=0;i<5;i++)
+			h.addToJobScheduler(pro[i]);
+		System.out.println(h.processes.size());
+		OS os = new OS(h);
+			int counter=0;
+		
+		while(true) {
+			
+			
+			    readyQueue2.add(h.processes.get(counter));
+				readyQueue2.get(counter).clock++;
+				 process min = readyQueue2.get(0);
+				 if(counter>0) {
+					 for(int i=0;i<readyQueue2.size();i++)
+						 if(readyQueue2.get(i).cpuBurst<min.cpuBurst)
+							 
+							 min=readyQueue2.get(i);
+					         min.numTimeCpu++;
+					 
+					 
+				 }
+			//cpu burst 
+		    	min.state.setRun();
+				 min.cpuBurst--;
+				
+				 for(int i=0;i<readyQueue2.size();i++)
+					 if(readyQueue2.get(i).cpuBurst==0) {
+						 timeTerminated=os.timeTerminatedOrKilled(readyQueue2.get(i));
+						 readyQueue2.remove(i); 
+					 } 
+				 if(counter<readyQueue2.size()-1)
+				    counter++;
+				
+						try {
+						 FileWriter fw=new FileWriter(new File("Allprocesses.txt"));
+						 PrintWriter pw=new PrintWriter(fw);
+							 pw.println("process id :"+" "+min.processId+"\n"+"process loaded at: "+" "+counter+"\n"+" Number of time it was in the cpu :"+" "+min.numTimeCpu);
+						pw.close();
+						}catch (Exception e) {System.out.println("File not found!");}
+						
+					 }
+
+					 
+	
+		    }
+		    
+		    
+				
+		}
+		
+
+	
+
+
